403f6496ea1601901cee61eee7960378509bee59
Felix Holfelder <felixholfelder1@t-online.de>
2024-07-04 11:14:46 +0200

    added icon

--- lib/service/AnimationService.dart
+++ lib/service/AnimationService.dart
@@ -1,15 +1,30 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:led_app/model/AnimationModel.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AnimationService {
+  static Future<File> _initializeFile() async {
+    final localDirectory = await getApplicationDocumentsDirectory();
+    final file = File("${localDirectory.path}/animations.json");
+
+    if (!await file.exists() || file.readAsStringSync() == "[]") {
+      final initialContent = await rootBundle.loadString("assets/animations.json");
+      await file.create();
+      await file.writeAsString(initialContent);
+    }
+
+    return file;
+  }
+
   static Future<List<AnimationModel>> getAnimations() async {
-    final String response = await rootBundle.loadString("assets/animations.json");
+    File file = await _initializeFile();
+    final String response = file.readAsStringSync();
     final List<dynamic> body = jsonDecode(response);
 
     List<AnimationModel> list = body.map((dynamic item) => AnimationModel.fromJson(item)).toList();
-
     list.sort((a, b) => a.name.compareTo(b.name));
     return list;
   }
